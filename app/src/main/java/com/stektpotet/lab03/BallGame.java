a14ee1f1d56157fc7028b1f1224750538bbed8ec
Halvor Bakken Smedås <stektpotet@gmail.com>
2018-02-27 22:40:04 +0100

    locked in landscape and basic layout setup

--- app/src/main/java/com/stektpotet/lab03/BallGame.java
+++ app/src/main/java/com/stektpotet/lab03/BallGame.java
@@ -1,13 +1,24 @@
 package com.stektpotet.lab03;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class BallGame extends AppCompatActivity {
+
+    private ConstraintLayout mBaseView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ball_game);
+        setContentView(R.layout.activity_game);
+        mBaseView = findViewById(R.id.main_baseView);
+        EnterImmersive();
+
+    }
+
+    private void EnterImmersive() {
+        mBaseView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE);
     }
 }
