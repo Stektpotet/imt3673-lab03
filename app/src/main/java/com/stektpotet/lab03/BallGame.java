8538ddeb0dbaabb3ecb8074239feb4fe0d17ae0f
Halvor Bakken Smedås <stektpotet@gmail.com>
2018-02-27 21:43:32 +0100

    Starting project...

---
+++ app/src/main/java/com/stektpotet/lab03/BallGame.java
@@ -0,0 +1,13 @@
+package com.stektpotet.lab03;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BallGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ball_game);
+    }
+}
